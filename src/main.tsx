45a8d31a8ced3f1b8374f6015a8b8f6fc680d4d2
sibisiddharth8 <sibisiddharth008@gmail.com>
2025-06-26 22:11:44 +0530

    dummy commit ci/cd test

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
-
 import clarity from '@microsoft/clarity';
 
 if (import.meta.env.PROD) {
