d756523c6933219c47789db51cf6f2e152265c22
sibisiddharth8 <sibisiddharth008@gmail.com>
2025-08-17 23:17:05 +0530

    removed temp datas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,6 @@ import ProjectDetailPage from '../pages/ProjectDetailPage';
 import TermsPage from '../pages/TermsPage';
 import NotFoundPage from '../pages/NotFoundPage';
 
-import PrivacyPolicy from '../temp/PrivacyPolicy';
-import CancellationRefundPolicy from '../temp/CancellationRefundPolicy';
-import ContactUs from '../temp/ContactUs';
-import ShippingPolicy from '../temp/ShippingPolicy';
 
 export const router = createHashRouter([
   {
@@ -38,26 +34,6 @@ export const router = createHashRouter([
             path: 'terms',
             element: <TermsPage />,
         },
-
-
-        {
-            path: 'privacy',
-            element: <PrivacyPolicy />,
-        },
-        {
-            path: 'cancellation',
-            element: <CancellationRefundPolicy />,
-        },
-        {
-            path: 'contact',
-            element: <ContactUs />,
-        },
-        {
-            path: 'shipping',
-            element: <ShippingPolicy />,
-        },
-
-        
         {
           path: '*',
           element: <NotFoundPage />,
