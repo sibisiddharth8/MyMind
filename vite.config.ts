753cdc1f32d645c6e8e18f31dcd24c055169d4ce
sibisiddharth8 <sibisiddharth008@gmail.com>
2025-09-29 20:12:21 +0530

    remove sitemap generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import sitemap from 'vite-plugin-sitemap'
-import getProjectRoutes from './sitemap-generator.js'
+// remove these two lines:
+// import sitemap from 'vite-plugin-sitemap'
+// import getProjectRoutes from './sitemap-generator.js'
 
-export default defineConfig(async ({ command }) => {
-  
-  const dynamicRoutes = command === 'build' ? await getProjectRoutes() : [];
-
-  return {
-    base: "/",
-    plugins: [
-      react(),
-      tailwindcss(),
-      sitemap({
-        hostname: 'https://sibisiddharth.me',
-        
-        // THIS IS THE FIX: We add the '/#' prefix to our static page routes.
-        // The root '/' stays as it is.
-        staticRoutes: ['/', '/#/projects', '/#/terms'],
-        
-        dynamicRoutes: dynamicRoutes,
-      }),
-    ],
-  }
+export default defineConfig({
+  base: "/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
