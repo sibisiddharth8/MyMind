21316cbf73114d016acff7fdd1c0f1c5746a589d
sibisiddharth8 <sibisiddharth008@gmail.com>
2025-09-29 21:26:29 +0530

    socket.io bug fix

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// console.log('BASE_URL:', BASE_URL);
 
-const SOCKET_URL = BASE_URL.replace('/api', '');
+const SOCKET_URL = BASE_URL.replace(/\/api$/, '');
+// console.log('SOCKET_URL:', SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
   path: '/api/socket.io',
